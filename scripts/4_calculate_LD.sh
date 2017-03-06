efafb7533e67a3e9eb018c9c92e5e33141fcccaf
ryan waples <ryan.waples@gmail.com>
2017-03-06 16:03:37 +0100

    basic outline

---
+++ scripts/4_calculate_LD.sh
@@ -0,0 +1,6 @@
+plink --bfile ./work/Koppen_ODD --r2 square --out ./work/Koppen_ODD
+plink --bfile ./work/Koppen_EVEN --r2 square --out ./work/Koppen_EVEN
+plink --bfile ./work/Nome_ODD --r2 square --out ./work/Nome_ODD
+plink --bfile ./work/Nome_EVEN --r2 square --out ./work/Nome_EVEN
+plink --bfile ./work/Puget_ODD --r2 square --out ./work/Puget_ODD
+plink --bfile ./work/Puget_EVEN --r2 square --out ./work/Puget_EVEN
