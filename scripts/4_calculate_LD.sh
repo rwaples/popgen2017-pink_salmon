df279ddb8f15f86f3f1fb5fd65af86994f133fc0
ryan waples <ryan.waples@gmail.com>
2017-03-07 22:22:18 +0100

    incomplete .sh files

--- scripts/4_calculate_LD.sh
+++ scripts/4_calculate_LD.sh
@@ -1,1 +1,6 @@
+/home/popgen2016/software/plink --bfile ./work/Koppen_ODD --r2 square --out ./work/Koppen_ODD
+/home/popgen2016/software/plink --bfile ./work/Koppen_EVEN --r2 square --out ./work/Koppen_EVEN
+/home/popgen2016/software/plink --bfile ./work/Nome_ODD --r2 square --out ./work/Nome_ODD
+/home/popgen2016/software/plink --bfile ./work/Nome_EVEN --r2 square --out ./work/Nome_EVEN
+/home/popgen2016/software/plink --bfile ./work/Puget_ODD --r2 square --out ./work/Puget_ODD
 /home/popgen2016/software/plink --bfile ./work/Puget_EVEN --r2 square --out ./work/Puget_EVEN
