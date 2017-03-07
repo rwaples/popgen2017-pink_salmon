b2d68332a9471bb3d1fbbe0a34ae07b00a9e9b25
ryan waples <ryan.waples@gmail.com>
2017-03-07 22:11:25 +0100

    added absolute path to plink to bypass alias

--- scripts/2_do_PCA.sh
+++ scripts/2_do_PCA.sh
@@ -1,2 +1,2 @@
-plink --bfile ./data/pink_salmon --autosome-num 26 --maf 0.1 --pca 3 --out ./work/pink_data.initial
-plink --bfile ./work/pink_salmon.clean --autosome-num 26 --maf 0.1 --pca 3 --out ./work/pink_salmon.clean
+/home/popgen2016/software/plink --bfile ./data/pink_salmon --autosome-num 26 --maf 0.1 --pca 3 --out ./work/pink_data.initial
+/home/popgen2016/software/plink --bfile ./work/pink_salmon.clean --autosome-num 26 --maf 0.1 --pca 3 --out ./work/pink_salmon.clean
--- scripts/4_calculate_LD.sh
+++ scripts/4_calculate_LD.sh
@@ -1,6 +1,1 @@
-plink --bfile ./work/Koppen_ODD --r2 square --out ./work/Koppen_ODD
-plink --bfile ./work/Koppen_EVEN --r2 square --out ./work/Koppen_EVEN
-plink --bfile ./work/Nome_ODD --r2 square --out ./work/Nome_ODD
-plink --bfile ./work/Nome_EVEN --r2 square --out ./work/Nome_EVEN
-plink --bfile ./work/Puget_ODD --r2 square --out ./work/Puget_ODD
-plink --bfile ./work/Puget_EVEN --r2 square --out ./work/Puget_EVEN
+/home/popgen2016/software/plink --bfile ./work/Puget_EVEN --r2 square --out ./work/Puget_EVEN
