b4c51b0d4226e47cd58d19473958b659cc7b4491
ryan waples <ryan.waples@gmail.com>
2017-03-07 19:40:45 +0100

    fix get_data

---
+++ scripts/0_get_data.sh
@@ -0,0 +1,3 @@
+wget http://people.binf.ku.dk/cetp/popgen/pink_salmon/pink_salmon.bed --directory-prefix ./data
+wget http://people.binf.ku.dk/cetp/popgen/pink_salmon/pink_salmon.bim --directory-prefix ./data
+wget http://people.binf.ku.dk/cetp/popgen/pink_salmon/pink_salmon.fam --directory-prefix ./data
